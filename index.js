eaa6dd6486338cf7098748abc75b05782d9bdc22
Gwin14 <fabiospinheira@gmail.com>
2023-08-19 11:49:51 -0300

    Vídeo 2
    Operação de insert

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ dotenv.config();
 
 (async () => {
   const db = require("./db");
+  console.log("Inserir novo cliente");
+  const nom = "jon";
+  const ida = 68;
+  await db.insereCliente({ nome: nom, idade: ida });
+
   console.log("Obter todos os clientes");
   const clientes = await db.todosClientes();
   console.log(clientes);
